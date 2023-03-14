afc74f0aea3ba0041c29ff780e9c4c847e09d716
Paras Kapoor <kapoorparas1997@gmail.com>
2023-03-14 20:37:57 +0530

    adding skeleton while loading

--- src/components/Row.js
+++ src/components/Row.js
@@ -9,7 +9,7 @@ const baseurl = `https://image.tmdb.org/t/p/original/`;
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const opts = {
     height: "390px",
@@ -21,42 +21,49 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const response = await axios.get(fetchUrl);
-      setMovies(response.data.results);
+      if (response.status === 200) {
+        setMovies(response.data.results);
+        setIsLoading(false);
+      }
     };
     fetchData();
   }, []);
 
   const handleClick = (movie) => {
-    // console.log("movie", movie);
-    // if (trailerUrl) {
-    //   setTrailerUrl("");
-    // } else {
-    //   movieTrailer(movie?.name || movie?.title || "")
-    //     .then((url) => {
-    //       const urlparams = new URLSearchParams(new URL(url).search);
-    //       setTrailerUrl(urlparams.get("v"));
-    //     })
-    //     .catch((err) => console.log(err));
-    // }
-    navigate("/player", { state: { movie } });
+    if (movie) {
+      navigate("/player", { state: { movie } });
+    }
   };
+  const arr = [];
+  arr.length = 5;
 
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className={`row__posters `}>
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            onClick={() => handleClick(movie)}
-            className={`row__poster ${isLargeRow && "row__posterlarge"}`}
-            src={`${baseurl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-            alt={movie.name}
-          />
-        ))}
+        {isLoading && (
+          <>
+            <div className="row__skeleton"></div>
+            <div className="row__skeleton"></div>
+            <div className="row__skeleton"></div>
+            <div className="row__skeleton"></div>
+            <div className="row__skeleton"></div>
+            <div className="row__skeleton"></div>
+          </>
+        )}
+        {!isLoading &&
+          movies.map((movie) => (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row__poster ${isLargeRow && "row__posterlarge"}`}
+              src={`${baseurl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+            />
+          ))}
       </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
